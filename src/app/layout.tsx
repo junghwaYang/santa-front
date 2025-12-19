15cb6a95a4e0b5090889899d6301d76ba4ed4fc1
tare <siltarre@gmail.com>
2025-12-19 22:01:08 +0900

    feat: add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -66,8 +66,8 @@ export const metadata: Metadata = {
     },
   },
   icons: {
-    icon: "/favicon.ico",
-    apple: "/apple-touch-icon.png",
+    icon: "/favicon.png",
+    apple: "/favicon.png",
   },
 };
 
